4fddd3dbcfae612392099199e4e9aa629e872bf6
ncs^ <parmarvishal8085@gmail.com>
2024-11-26 19:07:43 +0530

    second commit for jenkins

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Hello, Docker! Hello, world! This is  athe first file in local server."
+    return "Hello, Docker! Hello, world! This is a vishal parmar the first file in local server."
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
