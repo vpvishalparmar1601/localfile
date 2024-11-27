06c5251bde8a002468f89094d71ebc3f561a29b1
ncs^ <parmarvishal8085@gmail.com>
2024-11-27 16:45:48 +0530

    hello vishal parmar

--- main.py
+++ main.py
@@ -1,13 +1,11 @@
 from flask import Flask
 
-# Create a Flask app instance
 app = Flask(__name__)
 
-# Define the home route
 @app.route('/')
 def home():
-    return "Hello, Docker! Hello, world! This is Vishal Parmar's first file on a local server."
+    return 'Flask app is running!'
 
-# Ensure the app runs when the script is executed directly
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)  # Make the app accessible on all interfaces
+    # Change the host to 0.0.0.0 so it's accessible from outside the container
+    app.run(host='0.0.0.0', port=5000)
