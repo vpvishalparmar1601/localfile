400744bf90bdeecd64d90aeea5f8815d951d43f3
ncs^ <parmarvishal8085@gmail.com>
2024-11-27 20:09:33 +0530

    vishal vishal

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return 'hello my applicaton is deployed'
+    return 'hello my application is working'
 
 if __name__ == '__main__':
     # Change the host to 0.0.0.0 so it's accessible from outside the container
